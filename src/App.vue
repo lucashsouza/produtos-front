21e775d6b95251390f9b6f31e335843ebb522189
Lucas Souza <lhenriquesouza00@gmail.com>
2021-03-27 19:13:28 -0300

    VueJS - Consumindo API REST - #03 - Listado dados com Axios GET

--- src/App.vue
+++ src/App.vue
@@ -37,11 +37,10 @@
 
         <tbody>
 
-          <tr>
-
-            <td>Arduino</td>
-            <td>100</td>
-            <td>50.00</td>
+          <tr v-for="produto of produtos" :key="produto.id" >
+            <td>{{ produto.nome }}</td>
+            <td>{{ produto.quantidade }}</td>
+            <td>{{ produto.valor }}</td>
             <td>
               <button class="waves-effect btn-small blue darken-1"><i class="material-icons">create</i></button>
               <button class="waves-effect btn-small red darken-1"><i class="material-icons">delete_sweep</i></button>
@@ -59,6 +58,25 @@
 </template>
 
 <script>
+
+import Produto from './services/produtos'
+
+export default {
+
+  data(){
+    return {
+      produtos: []
+    }
+  },
+
+  mounted(){
+    Produto.listar().then(resposta => {
+      console.log(resposta.data)
+      this.produtos = resposta.data
+    })
+  }
+
+}
 </script>
 
 <style>
